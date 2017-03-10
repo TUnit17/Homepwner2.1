c229ca7ed17293d1a3071929c5b0d105a1c349ee
TUnit17 <thuang177@gmail.com>
2017-03-09 22:07:33 -0800

    Something broke here

---
+++ Homepwner2/ItemCell.swift
@@ -0,0 +1,22 @@
+//
+//  ItemCell.swift
+//  Homepwner2
+//
+//  Created by Timothy Huang on 3/9/17.
+//  Copyright © 2017 Timothy Huang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemCell: UITableViewCell
+{
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel! 
+
+
+
+
+
+} // end class
